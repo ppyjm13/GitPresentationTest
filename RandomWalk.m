2a0abd5d985dab78e93baea396da99d18c30f99e
Jamie Mair <jamie.mair@hotmail.co.uk>
2020-05-13 18:49:05 +0100

    Added a random walk loop which does the simulation

--- RandomWalk.m
+++ RandomWalk.m
@@ -7,3 +7,13 @@ initial_x = 0;
 initial_t = 0;
 probability_up = 0.5;
 probability_down = 0.5;
+x = zeros(1, final_t-initial_t);
+t = initial_t:final_t;
+x(1) = initial_x;
+for i=2:len(x)
+    if rand < probability_up
+       x(i) = x(i-1) + 1; 
+    else
+       x(i) = x(i-1) - 1; 
+    end
+end
