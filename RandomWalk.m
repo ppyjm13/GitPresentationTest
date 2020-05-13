d3be9bf151f1c8659d719e94b3f0f10673cbcf82
Jamie Mair <jamie.mair@hotmail.co.uk>
2020-05-13 18:41:35 +0100

    Created random walk script and added simulation parameters

---
+++ RandomWalk.m
@@ -0,0 +1,9 @@
+%% Author: Jamie Mair
+% Random walk script to simulate a single random walk with parameters
+
+
+walk_length = 20;
+initial_x = 0;
+initial_t = 0;
+probability_up = 0.5;
+probability_down = 0.5;
