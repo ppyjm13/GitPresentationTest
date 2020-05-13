1b2692a484d5aa0fee74cb194c7edb7173ef39dc
Jamie Mair <jamie.mair@hotmail.co.uk>
2020-05-13 18:49:42 +0100

    Changed the way some parameters are displayed

--- RandomWalk.m
+++ RandomWalk.m
@@ -2,11 +2,12 @@
 % Random walk script to simulate a single random walk with parameters
 
 
-walk_length = 20;
 initial_x = 0;
 initial_t = 0;
+final_t = 20;
 probability_up = 0.5;
-probability_down = 0.5;
+probability_down = 1.0 - probability_up;
+
 x = zeros(1, final_t-initial_t);
 t = initial_t:final_t;
 x(1) = initial_x;
